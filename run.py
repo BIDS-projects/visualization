6b7c581cc91cf4c5b965dc32238b86adb9082571
Alvin Wan <hi@alvinwan.com>
2016-02-14 02:35:02 -0800

    kwarg

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from visualization import app
 import os
 
 if __name__ == '__main__':
-    app.run(int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(port=int(os.environ.get('PORT', 5000)), debug=True)

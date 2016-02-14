6ccfe28505694f7507ea2c426d9c47030a9faba0
Alvin Wan <hi@alvinwan.com>
2016-02-14 02:48:42 -0800

    added host

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from visualization import app
 import os
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))

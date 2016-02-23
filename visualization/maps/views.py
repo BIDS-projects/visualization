2e1ece25daafc6cd7752e8119f5654dc267c2a92
Alvin Wan <hi@alvinwan.com>
2016-02-22 19:21:09 -0800

    fixed edge translations and weight normalization

--- visualization/maps/views.py
+++ visualization/maps/views.py
@@ -25,7 +25,20 @@ def pull_map_url(endpoint, values):
 
 @maps.route('/')
 def home():
+    mapper = {}
     graph = Graph.query.filter_by(name=g.map_url).one()
+    vertices = Vertex.query.filter_by(graph_id=graph.id).all()
+    for i, vertex in enumerate(vertices):
+        vertex.i = i
+        mapper[vertex.id] = i
+    edges = Edge.query.filter_by(graph_id=graph.id).all()
+    most = max(e.weight for e in edges)
+    least = min(e.weight for e in edges)
+    diff = most - least
+    for edge in edges:
+        edge.from_v = mapper[edge.from_id]
+        edge.to_v = mapper[edge.to_id]
+        edge.weight = 10*((edge.weight - least)/diff)
     return render_template('topicsmap.html',
-        vertices=Vertex.query.filter_by(graph_id=graph.id).all(),
-        edges=Vertex.query.filter_by(graph_id=graph.id).all())
+        vertices=vertices,
+        edges=edges)
